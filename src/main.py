a29757df9d62d0d73910d89e95e7103d3e305597
Timofey <tima.khlystov99@mail.ru>
2024-09-10 05:22:25 +0300

    Добавлены методы по валидации и форматированию данных из списка вакансий

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
+import json
+
 from src.vacancy_api import HeadHunterAPI, SuperJobAPI
 
 if __name__ == "__main__":
     search_query = input("Введите поисковый запрос: ").capitalize()
 
-    # Поиск вакансий по запросу с API
     hh_api = HeadHunterAPI()
     sj_api = SuperJobAPI()
-    hh_vacancies = hh_api.get_vacancies(search_query)
+
+    # Получение списка вакансий по API
+    hh_vacancies_list = hh_api.get_vacancies(search_query)
     sj_vacancies = sj_api.get_vacancies(search_query)
 
+    # Валидация полученных вакансий
+    hh_val_vacancies = hh_api.hh_validations_vacancies(hh_vacancies_list)
+    sj_val_vacancies = sj_api.sj_validations_vacancies(sj_vacancies)
+
+    # приведение к общим ключам вакансий с платформ
+    vacancies_hh = hh_api.hh_data_formatting(hh_val_vacancies)
+    vacancies_sj = sj_api.sj_data_formatting(sj_val_vacancies)
+
+
+    # print(json.dumps(vacancies_sj, indent=2, ensure_ascii=False))
 
