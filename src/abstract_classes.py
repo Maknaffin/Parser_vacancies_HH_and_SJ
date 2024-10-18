9e80fa162d552c361522ac38ce946f272e7945ad
Timofey <tima.khlystov99@mail.ru>
2024-10-18 23:39:18 +0300

    Добавлены абстрактный метод для форматирования данных

--- src/abstract_classes.py
+++ src/abstract_classes.py
@@ -5,7 +5,7 @@ class VacancyAPI(ABC):
     """Абстрактный класс для работы с API"""
 
     @abstractmethod
-    def get_vacancies(self, keyword):
+    def get_vacancies(self, search_query):
         """Абстрактный метод для получения вакансий"""
         pass
 
@@ -13,3 +13,9 @@ class VacancyAPI(ABC):
     def validations_vacancies(self, vacancies_list):
         """Абстрактный метод для валидации данных из списка вакансий"""
         pass
+
+    @abstractmethod
+    def data_formatting(self, val_vacancies):
+        """Абстрактный метод для форматирования данных из вакансий по общим ключам"""
+        pass
+
